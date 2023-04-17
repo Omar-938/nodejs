bc21ba57b5aa983257da08ca506ec36f9909e3a1
BENSADIA Omar (SNCF RESEAU / EIC PARIS NORD / Siège UO NIF) <9505883g@commun.ad.sncf.fr>
2023-04-17 10:40:36 +0200

    test

--- index.js
+++ index.js
@@ -3,3 +3,4 @@ console.log(chalk.blue("Hello Jamal !!"));
 console.log(chalk.red("Hello twafik !!"));
 console.log(chalk.yellow("Hello Tophe !!"));
 console.log(chalk.green("Hello Aurelie !!"));
+console.log("coucou");
