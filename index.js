f14ee1b98bce1f49cda2e0bc1c0249686521290b
BENSADIA Omar (SNCF RESEAU / EIC PARIS NORD / Siège UO NIF) <9505883g@commun.ad.sncf.fr>
2023-04-17 10:48:26 +0200

    modification nom tawfik

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import chalk from 'chalk';
 console.log(chalk.blue("Hello Jamal !!"));
-console.log(chalk.red("Hello twafik !!"));
+console.log(chalk.red("Hello tawfik !!"));
 console.log(chalk.yellow("Hello Tophe !!"));
 console.log(chalk.green("Hello Aurelie !!"));
 console.log("coucou");
